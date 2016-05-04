18b844978ceb483b31e1db2f64be3fe6560008fb
Rupinderjeet <rupinderjeet47@gmail.com>
2016-05-04 13:03:02 +0530

    update-blog

--- js/index.js
+++ js/index.js
@@ -198,6 +198,27 @@ window.onload = function () {
 //GLOBAL VARIABLES
 var slideShow;              // used by about page's 'play' button as an interval
 
+function settings(caller, type){
+    var color_name;
+
+    switch(type){
+        case 'color':
+            color_name = $(caller).val().toLowerCase();
+            if(color_name === 'default'){
+                document.body.style.color = '#333333';
+            } else {  document.body.style.color = color_name;  }
+            break;
+        case 'bg-color':
+            color_name = $(caller).val().toLowerCase();
+            if(color_name === 'default'){
+                document.body.style.backgroundColor = '#333333';
+            } else {  document.body.style.backgroundColor = color_name;  }
+            break;
+
+        default: console.log('bad settings');
+    }
+}
+
 // 'on-event' function methods
 if (checkInternet()) {
     $('#report-issue-modal').on('hidden.bs.modal', function (e) {
@@ -210,3 +231,4 @@ if (checkInternet()) {
         $('#attendance-modal-body').load('index.html #attendance-modal-body', null, null);
     });
 }
+
